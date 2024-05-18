36c1a7284f5d49e7fe0eb04a10c4a2d0f86d3ad1
Fernando Rodríguez Sela <fernando@ferimer.es>
2024-05-18 18:14:35 +0200

    Esqueleto para examen

---
+++ src/app/backoffice/page.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+      <>
+        <h1>CodeMasters Dashboard</h1>
+        <p>Bienvenido al Dashboard de CodeMasters</p>
+        <p>¡ Gestiona los cursos desde aquí !</p>
+        <p>Selecciona una sección en el menú de la izquierda</p>
+      </>
+  );
+}
